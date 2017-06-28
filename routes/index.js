e3596297e1446aa9c2db52692ea337b885be194a
Haeseong Jeon <seong0428@gmail.com>
2017-06-28 22:37:57 +0900

    index.js, users.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var htmlparser = require('htmlparser2');
+var select = require('soupselect').select;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // request('http://www.google.com', function (error, response, body) {
+  request('https://www.acmicpc.net/user/junhaesung', function (error, response, body) {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log('hello!');
+    parseHTML(body);
+
+  });
+
   res.render('index', { title: 'Express' });
 });
+
+function parseHTML(body) {
+  var handler = new htmlparser.DomHandler(function (error, dom) {
+    if (error)
+    	console.log(error);
+    else {
+      var sel = select(dom, 'body div div div div div div div div div div span a');
+    }
+  });
+  var parser = new htmlparser.Parser(handler);
+  parser.write(body);
+  parser.end();
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,47 @@ var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  var name = req.query.name;
-  var db = firebase.database();
-  db.ref('/users/' + name );
+  var ref = firebase.database().ref('/users');
   ref.once('value', function(snapshot) {
     console.log(snapshot.val());
+    res.status(200).json(snapshot.val());
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
   });
+});
+
+router.get('/:id', function(req, res, next) {
+  var ref = firebase.database().ref('/users/' + req.params.id);
+
+  ref.once('value', function(snapshot) {
+    console.log(snapshot.val());
+    res.status(200).json(snapshot.val());
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  // req.body.bid, req.body.name
+  var data = req.body;
+  data.problems = [];
+  console.log(data);
+
+  firebase.database().ref('/users')
+  .push(data)
+  .then(function(result) {
+    console.log(result);
+    res.status(200).send('success');
+  })
+  .catch(function(error) {
+    res.status(500).send(error);
+  });
+});
+
+router.put('/', function(req, res, next) {
   res.status(200).send('success');
 });
 
